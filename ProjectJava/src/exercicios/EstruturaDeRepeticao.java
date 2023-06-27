28fd538a971392df3e39ba27909f07db097e8c27
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-06-27 17:54:28 -0300

    EstruturaDeRepeticao

---
+++ ProjectJava/src/exercicios/EstruturaDeRepeticao.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class EstruturaDeRepeticao {
+	public static void main(String[] args) {
+	
+	for (int numeroTabuada = 1; numeroTabuada <= 10; numeroTabuada++) {
+		System.out.println("Tabuada do " + numeroTabuada + ":");
+		for (int numeroMultiplicado  = 1; numeroMultiplicado <= 10; numeroMultiplicado++) {
+			int resultado = numeroTabuada * numeroMultiplicado;
+            System.out.println( numeroTabuada + " x " + numeroMultiplicado + " = " + resultado);
+        }
+        System.out.println(); // Linha em branco para separar as tabuadas
+	}
+
+  }
+}
