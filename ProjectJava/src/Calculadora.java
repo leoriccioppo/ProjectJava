4f516ed43b7ee352fb030cc87447dbd634b2261f
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-06-30 13:52:59 -0300

    acertosCalculadora

--- ProjectJava/src/Calculadora.java
+++ ProjectJava/src/Calculadora.java
@@ -1,36 +1,59 @@
 public class Calculadora {
-    private double numero1;
-    private double numero2;
-    private char operador;
+	private double numero1, numero2;
+	private char operador;
 
     public double calcular() {
+        
+    	double resultado = 0;
+    	
         switch (operador) {
             case '+':
-                return numero1 + numero2;
+                resultado = somar(numero1,numero2);
+                break;
             case '-':
-                return numero1 - numero2;
+            	resultado = subtracao(numero1,numero2);
+                break;
             case '*':
-                return numero1 * numero2;
+            	resultado = multiplicacao(numero1,numero2);
+                break;
             case '/':
                 if (numero2 != 0) {
-                    return numero1 / numero2;
+                	resultado = divisao(numero1,numero2);
+                    break;
                 } else {
                     throw new ArithmeticException("Erro: divisão por zero!");
                 }
             default:
-                throw new IllegalArgumentException("Operação inválida!");
+               throw new IllegalArgumentException("Operação inválida!");      
         }
+        return resultado;
     }
+    
+    private double somar (double numero1, double numero2) {
+    	return numero1 + numero2;
+    };
+    
+    private double subtracao (double numero1, double numero2) {
+    	return numero1 - numero2;
+    };
+    
+    private double multiplicacao (double numero1, double numero2) {
+    	return numero1 * numero2;
+    };
+    
+    private double divisao (double numero1, double numero2) {
+    	return numero1 / numero2;
+    };
 
     public void setNumero1(double numero1) {
         this.numero1 = numero1;
-    }
+    };
 
     public void setNumero2(double numero2) {
         this.numero2 = numero2;
-    }
+    };
 
     public void setOperador(char operador) {
         this.operador = operador;
-    }
+    };
 }
