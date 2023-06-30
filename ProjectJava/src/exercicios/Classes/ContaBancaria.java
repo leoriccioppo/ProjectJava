ca872424c0e4855490a5ceeacef236e7f8a21a8e
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-06-30 14:21:09 -0300

    operacoesAtualizadas

--- ProjectJava/src/exercicios/Classes/ContaBancaria.java
+++ ProjectJava/src/exercicios/Classes/ContaBancaria.java
@@ -16,48 +16,82 @@ public class ContaBancaria {
 		}
 		
 		
-		private void depositar(double valor) {
-		System.out.println("Qual valor gostaria de depositar?");
-	    valor = input.nextDouble();
-		saldo+=valor;
-		}
 		
-	    private void sacar(double valor) {
-
-	        if (valor <= saldo) {
-	            saldo -= valor;
-	        } else {
-	            System.out.println("Saldo insuficiente para saque.");
+public void depositar(double valor) {
+			try {	
+				System.out.println("Qual valor gostaria de depositar?");
+			    valor = input.nextDouble();
+				saldo+=valor;
+				System.out.println("Depósito realizado com sucesso.");
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Valor inválido. Certifique-se de digitar um número decimal válido.");
+        }
+}
+		
+	    
+		
+public void sacar(double valor) {
+			try {
+				System.out.print("Qual valor gostaria de sacar? ");
+	            valor = input.nextDouble();
+	            if (valor <= saldo) {
+		            saldo -= valor;
+		        } else {
+		            System.out.println("Saldo insuficiente para saque.");
+		        }
+			} catch (java.util.InputMismatchException e) {
+	            System.out.println("Valor inválido. Certifique-se de digitar um número decimal válido.");
 	        }
 	    }
 
-	    private void exibirSaldo() {
+
+
+public void exibirSaldo() {
 	        System.out.println("Saldo atual: " + saldo);
 	    }
 	    
-	    public void operacoes () {
-	       System.out.println("Digite a operação desejada (1 \"Depositar\", 2 \"Sacar\", 3 \"Exibir Saldo\", 5 \"Sair\"): ");
+	
+
+public void operacoes () {
+	boolean continuar = true;
+	
+	while (continuar) {
+
+	       System.out.println("Digite a operação desejada (1 \"Depositar\", 2 \"Sacar\", 3 \"Exibir Saldo\", 4 \"Sair\"): ");
            int operador = input.nextInt();
         
 	    	
 	    	switch(operador) {
 	    case 1:
-             depositar(5.0);
+             depositar(0.0);
            break;    
 	    case 2:
-            sacar(50.0);
+            sacar(0.0);
             break;
             
 	    case 3:
 	    	exibirSaldo();
 	    	break;
-        default:
+	    
+	    case 4:
+	    	continuar = false;
+            System.out.println("Encerrando operações.");
+            break;
+        
+	    default:
             System.out.println("Operação inválida.");
             break;
 	    }
-	    
+	   } 
 	}
-	    
+	   
+
+
+
+public static void main(String[] args) {
+	        ContaBancaria conta = new ContaBancaria(12345);
+	        conta.operacoes();
+	    }    
 	    
  }
 	
