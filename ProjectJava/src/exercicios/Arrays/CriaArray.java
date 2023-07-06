92695810b6d929f2a31e2e582302d83cbf93a5c4
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-07-06 20:16:24 -0300

    end1

--- ProjectJava/src/exercicios/Arrays/CriaArray.java
+++ ProjectJava/src/exercicios/Arrays/CriaArray.java
@@ -1,11 +1,25 @@
 package exercicios.Arrays;
+import java.util.Scanner;
+
 
 public class CriaArray {
-    
+    static int[] numeros = new int[8];
+    static Scanner input = new Scanner(System.in);
 
 
     public static void main(String[] args) {
-	       
+	       for (int i=0; i < numeros.length; i++){
+            System.out.println("Digite o " +(i+1)+ "° número:");
+            numeros[i] = input.nextInt();
+           }
+
+           int resultado=0;
+
+           for(int i=0; i < numeros.length; i++){
+            resultado+=numeros[i];
+           }
+
+           System.out.println("A soma dos números é:" + resultado);
  }    
 }
 
