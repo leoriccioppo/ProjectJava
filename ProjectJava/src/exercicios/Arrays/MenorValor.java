222396d4b0118a20cb5cbaf3cba1bd9b00361f41
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-07-07 15:59:26 -0300

    menorNumero

--- ProjectJava/src/exercicios/Arrays/MenorValor.java
+++ ProjectJava/src/exercicios/Arrays/MenorValor.java
@@ -11,7 +11,18 @@ public class MenorValor {
         //pergunta os números e armazena
         System.out.println("Escolha 10 números, e eu te respondo qual foi o menor!!");
 
-        
+        for (int numero =0; numero < numeros.length; numero++){
+            System.out.println("Digite o " +(numero+1)+"° número:");
+            numeros[numero] = input.nextInt();
+        }
 
+        int menorNumero=numeros[0];
+        for( int numero : numeros){
+            if (numero < menorNumero){
+                menorNumero = numero;
+            }
+        }
+
+         System.out.println("O menor número é:" +menorNumero);
     }
 }
