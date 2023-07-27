a43a21547e028c432e81fde2c90d9e078bb7e632
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-07-27 20:40:51 -0300

    estoque

---
+++ ProjectJava/src/exercicios/LivrariaOnline/manipulacao/Estoque.java
@@ -0,0 +1,30 @@
+package exercicios.LivrariaOnline.manipulacao;
+
+import exercicios.LivrariaOnline.livro.Ebook;
+import exercicios.LivrariaOnline.livro.Livro;
+import exercicios.LivrariaOnline.livro.LivroFisico;
+
+public class Estoque extends ManipulaçãoLivros {
+
+    public Estoque() {
+        super();
+
+        livrosIniciais();
+    
+    }
+
+    private void livrosIniciais() {
+        Ebook livroEbook1 = new Ebook("Café com Deus pai", "Junior Rostirola", "ISBN 1", 29.90, 2.5);
+        LivroFisico livroFisico1 = new LivroFisico("É assim que acaba", "Colleen Hoover", "ISBN 2", 39.90, 1.2, 5.00);
+        LivroFisico livroFisico2 = new LivroFisico("O poder da autorresponsabilidade", "Paulo Vieira", "ISBN 3", 49.90, 1.2, 5.00);
+        LivroFisico livroFisico3 = new LivroFisico("Onde estão as flores", "Ilko Minev", "ISBN 4", 49.90, 1.2, 5.00);
+        Ebook livroEbook2 = new Ebook("Rita Lee - outra autobiografia", "Rita Lee", "ISBN 5", 19.90, 2.5);
+    
+        adicionarLivro(livroEbook1);
+        adicionarLivro(livroFisico1);
+        adicionarLivro(livroFisico2);
+        adicionarLivro(livroFisico3);
+        adicionarLivro(livroEbook2);
+    }
+    
+}
